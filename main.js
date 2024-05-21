3ad5498a29fecac679cfb7219e952793e3d14443
asuna26 <108690133+asuna26@users.noreply.github.com>
2024-05-21 12:21:51 -0300

    a

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const dataAtual = new Date ();
 let dataObjetivo = prompt("Data final ANO-MES-DIA Exemplo: 2024-04-10");
 dataObjetivo = new Date (dataObjetivo + "T23:59:59");
 let diasQueFaltam = Math.floor ((dataObjetivo - dataAtual) / 8640000);
-document.querySelector("#dia_restantes"). textContent = diasQueFaltam;
+document.querySelector("#dia_restantes").textContent = diasQueFaltam;
